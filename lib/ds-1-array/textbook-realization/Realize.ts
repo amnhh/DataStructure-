6758ef11619900e1e95f8c8f6ac3b1cbc0e4d9f5
amnhh <2570079254@qq.com>
2020-05-26 08:46:55 +0800

    feat

--- lib/ds-1-array/textbook-realization/Realize.ts
+++ lib/ds-1-array/textbook-realization/Realize.ts
@@ -1,67 +1,93 @@
-export default interface IArray <T> {
-    data: Array<T>
+import IArray from './Interface'
+
+export default class implements IArray<number> {
+    // 数据集合
+    _data: Array<number> = []
+
+    // 容量
+    _capacity: number = 0
+
+    _size: number = 0
+
+    // 构造器
+    constructor(capacity: number, initValue: number = 0) {
+        this._data = new Array<number>()
+        this._capacity = capacity
+
+        for (; this._size < capacity; this._size ++) {
+            this._data[this._size] = initValue
+        }
+    }
 
     /**
      * 元素总和
      *
      * @desc 报告数组当前的规模
      */
-    size(): number
+    size(): number {
+        return this._size
+    }
 
     /**
      * 获取索引为 r 的元素
      */
-    get(r: number): T
+    get(r: number): number {
+        return this._data[r]
+    }
 
     /**
      * 用 e 替换索引为 r 的数值
      */
-    put(r: number, e: T): void
+    put(r: number, e: number): void {
+        this._data[r] = e
+    }
 
     /**
      * 将 e 插入到索引为 r 的位置上，其他的后继元素依次向后挪
      */
-    insert(r: number, e: T): void
+    insert(r: number, e: number): void {
+
+    }
 
     /**
      * 判断是否数组已有序
      */
-    disordered(): boolean
+    disordered(): boolean {}
 
     /**
      * 升序排列
      */
-    sort(): void
+    sort(): void {}
 
     /**
      * 找到值为 e 的元素
      * 存在两个或两个以上时，会返回最大的索引
      */
-    find(e: T): number
+    find(e: T): number {}
 
     /**
      * 查找目标元素 e，返回不大于 e，且索引值最大的元素
      *
      * @desc 该方法，就是为了找到如果说想要将 e 插入到该数组，该插入到哪里
      */
-    search(e: T): number
+    search(e: T): number {}
 
     /**
      * 剔除重复的元素
      *
      * @desc 针对的是无序数组
      */
-    deduplicate(): void
+    deduplicate(): void {}
 
     /**
      * 剔除重复的元素
      *
      * @desc 针对有序数组
      */
-    uniquify(): void
+    uniquify(): void {}
 
     /**
      * 遍历数组，数组中的元素全部执行 cb 方法
      */
-    traverse(cb: Function): void
+    traverse(cb: Function): void {}
 }
