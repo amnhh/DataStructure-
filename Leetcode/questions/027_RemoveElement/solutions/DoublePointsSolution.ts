2ab0112d0fb69ed856c40a96a99f3d8406df8e26
amnhh <2570079254@qq.com>
2020-06-16 15:14:44 +0800

    comment 位置错误修正

--- Leetcode/questions/027_RemoveElement/solutions/DoublePointsSolution.ts
+++ Leetcode/questions/027_RemoveElement/solutions/DoublePointsSolution.ts
@@ -1,22 +1,27 @@
 /**
- * @title 标题
+ * @title
+ *  双指针法
  *
- * @index 次序
+ * @index
+ *  1
  *
- * @trainOfThought 思路
+ * @trainOfThought
+ *  维护一个 i 用来标识当前不重复的最后一个索引，维护一个 j 连做遍历器
+ *  遇到 j === val 则跳过，否则写入 i 这个坑位，i 自增
  *
- * @complexAnalysis 复杂度分析
+ * @complexAnalysis
+ *  空间复杂度: i, j 两个变量 ~  O(2) => O(1)
+ *  时间复杂度: j 最多执行 n 次便会停止 ~ O(n)
  *
- * @type 解法类型
+ * @type
+ *  双指针
+ *  快慢指针
  *
- * @note 笔记
- *
- * @remark 备注
- *
- * @category 想要分配到哪个大类下
+ * @category
+ *  Array
  *
  * @score 时间、内存消耗得分
- * 执行用时击败 T ，内存消耗击败 T
+ * 执行用时68 ms击败 68.65% ，内存消耗32.9 MB击败 100.00%
  */
 
 export default function DoublePointsSolution (nums: number[], val: number): number {
