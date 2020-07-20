0b917c2b4b106d6816c8738e68af93180c4f5ce9
amnhh <2570079254@qq.com>
2020-07-20 10:23:58 +0800

    add comment

--- IntroductionToAlgorithm/videos/4__QuickSort/problems/QuickSort/realize.ts
+++ IntroductionToAlgorithm/videos/4__QuickSort/problems/QuickSort/realize.ts
@@ -26,6 +26,9 @@ function partition (A: number[], start: number, end: number) {
         }
     }
 
+    // 除去 start 位，我们保证 [start + 1, i] 位都比 A[start] 小
+    // 所以交换 start 和 i 位，即可保证 start 位上的数字存在于它最终的位置
+    // 循环不变式变为 [0, i - 1] 位均小于 pivotValue
     swap(A, i, start)
 
     return i
